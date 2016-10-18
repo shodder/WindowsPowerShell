e20488a699733f745fc1e8698f9ec2b09d437ea1
Simon Hodder <simon.hodder@stfc.ac.uk>
2016-10-18 09:13:08 +0100

    Add new short cut alias

--- aliases.ps1
+++ aliases.ps1
@@ -11,3 +11,7 @@ Set-Alias which Get-Command
 $workspace = Join-Path -Path $HOME -ChildPath 'workspace'
 $isis = Join-Path -Path $workspace -ChildPath 'isisbusapps'
 $si = Join-Path -Path $isis -ChildPath 'sisites'
+
+$psConfig = Join-Path -Path $HOME -ChildPath 'Documents/WindowsPowerShell'
+
+$dotfiles = Join-Path -Path $HOME -ChildPath 'dotfiles'
