ee328b094ab39d7d7c6a7206b2e77db4cb43bed5
Simon Hodder <simon.hodder@stfc.ac.uk>
2016-10-21 09:43:03 +0100

    Added basic function to launch Remote Desktop Conn

--- sp-funcs.ps1
+++ sp-funcs.ps1
@@ -2,16 +2,34 @@
 ###########################
 # SHAREPOINT
 ###########################
-$DevServer2 = 'fitnlabsspdev2.isis.cclrc.ac.uk'
 
-Function Enter-SP([String]$server=$DevServer2)
-{
+$servers = @{
+    'devfe' = 'fitnlabsspdev1.isis.cclrc.ac.uk';
+    'dev2' = 'fitnlabsspdev2.isis.cclrc.ac.uk';
+    'devapp' = 'fitnlabsspdev3.isis.cclrc.ac.uk';
+    'devsql' = 'fitnlabsqldev1.isis.cclrc.ac.uk';
+    'prodsql' = 'fitnlabsqldev1.isis.cclrc.ac.uk'
+}
+
+Function Enter-SP {
+    param(
+      [String]$ServerName='dev2'
+    )
+
+    $server=$servers.Get_Item($ServerName)
+    Write-Host "Server: $server"
     Enter-PSSession -computername $server
 }
 
-Function Add-SP
-{
-    Add-PSSnappin microsoft.sharepoint.powershell
-}
+Function Start-SPRemote {
+    param(
+      [String]$ServerName='dev2',
+      [String]$UserName='clrc\mhr39349'
+    )
 
+    $server=$servers.Get_Item($ServerName)
+
+    Start-Process "$env:windir\system32\mstsc.exe" -ArgumentList "/v:$server"
+
+}
 
