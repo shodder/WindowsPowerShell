1de58a6093cb3c1f7cf0722021b7b86acc83f804
Simon Hodder <simon.hodder@stfc.ac.uk>
2017-02-10 09:18:20 +0000

    Fixes style issues

--- vim-funcs.ps1
+++ vim-funcs.ps1
@@ -6,16 +6,23 @@
 $VimPath = "C:/gvim/./vim.exe"
 set-alias vim $VimPath
 
+function view {
+    Param(
+        [parameter(Mandatory=$true)]
+        [String] $FilePath
+    )
+
+    vim $FilePath
+}
+
 # To edit the Powershell Profile
 # (Not that I'll remember this)
-Function Edit-Profile
-{
+Function Edit-Profile {
     vim $profile
 }
 
 # To edit Vim settings
-Function Edit-Vimrc
-{
+Function Edit-Vimrc {
     vim $HOME\.vimrc
 }
 
