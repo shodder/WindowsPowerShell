90fc3a4768b7e0c576cadca53e4d0524a4e8f19d
Simon Hodder <simon.hodder@stfc.ac.uk>
2017-04-04 15:42:16 +0100

    Updated aliases

--- aliases.ps1
+++ aliases.ps1
@@ -41,6 +41,7 @@ $workspace = Join-Path -Path $HOME -ChildPath 'workspace'
 $isis = Join-Path -Path $workspace -ChildPath 'isisbusapps'
 $si = Join-Path -Path $isis -ChildPath 'sisites'
 
-$psConfig = Join-Path -Path $HOME -ChildPath 'Documents/WindowsPowerShell'
+$ps = Join-Path -Path $HOME -ChildPath 'Documents/WindowsPowerShell'
+$dots = Join-Path -Path $HOME -ChildPath 'dotfiles'
 
-$dotfiles = Join-Path -Path $HOME -ChildPath 'dotfiles'
+
