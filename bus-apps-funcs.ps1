a7d5a2a093003241c1135bef61618d7c80ca2f7f
Simon Hodder <hodder.simon@gmail.com>
2018-03-08 12:00:27 +0000

    Add payara domain restart command

--- bus-apps-funcs.ps1
+++ bus-apps-funcs.ps1
@@ -14,3 +14,12 @@ function Stop-Domain {
     Set-Location "C:\payara\installations\payara-4.1.2.172\payara41\bin"
     .\asadmin.bat stop-domain --domaindir C:\payara\domains "domain$DomainNumber"
 }
+
+function Restart-Domain {
+    param(
+        [String]$DomainNumber="1"
+    )
+    Set-Location "C:\payara\installations\payara-4.1.2.172\payara41\bin"
+    .\asadmin.bat stop-domain --domaindir C:\payara\domains "domain$DomainNumber"
+    .\asadmin.bat start-domain --domaindir C:\payara\domains "domain$DomainNumber"
+}
