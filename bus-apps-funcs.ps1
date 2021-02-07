006711a7bda35b8afe63dd4309a930c6d6954e25
Simon Hodder <simon.hodder@stfc.ac.uk>
2021-02-07 16:08:05 +0000

    Updated paths for new PC

--- bus-apps-funcs.ps1
+++ bus-apps-funcs.ps1
@@ -98,40 +98,13 @@ function Start-VisualStudioAsAdmin2017 {
 
 New-Alias -Name vsadmin17 -Value Start-VisualStudioAsAdmin2017 -Force -Option AllScope
 
-function Launch-Me {
-    & 'C:\Program Files\Mozilla Firefox\firefox.exe' `
-        'https://mail.protonmail.com/inbox' `
-        'https://twitter.com/' `
-        'https://www.yammer.com/stfc.ac.uk/#/home'
-}
-
-function Launch-Outlook {
-    & 'C:\Program Files (x86)\Microsoft Office\Office16\OUTLOOK.EXE'
-}
-
-function Launch-Planners {
-   & 'C:\Program Files\Mozilla Firefox\firefox.exe' `
-        'https://trello.com/b/o1PxTdzd/init' `
-        'https://github.com/orgs/isisbusapps/projects/9?card_filter_query=author%3Ashodder' `
-        'https://tasks.office.com/stfc365.onmicrosoft.com/en-US/Home/Planner' `
-        'https://trello.com/b/7q0YQ93H/slm-board'
-}
-
-function Launch-CommonApps {
-    Launch-Me
-    Launch-Planners
-    Launch-Outlook
-    exit
-}
-
-New-Alias -Name launch -Value Launch-CommonApps -Force -Option AllScope
 
 ###########################
 # Shortcuts
 ###########################
 $workspace = Join-Path -Path $HOME -ChildPath 'workspace'
 $scratch = Join-Path -Path $workspace -ChildPath 'scratch'
-$busapps = Join-Path -Path $workspace -ChildPath 'isisbusapps'
+$busapps = 'C:\Programming'
 $si = Join-Path -Path $busapps -ChildPath 'sisites'
 $users = Join-Path -Path $busapps -ChildPath 'Users'
 $userofficetools = Join-Path -Path $busapps -ChildPath 'UserOfficeTools'
